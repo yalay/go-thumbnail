bc4b63a2ae9e6b3850060253163b39290683f256
yalay <ya_lay@163.com>
2016-05-04 22:01:35 +0800

    fix:修复cookie设置时效性处理

--- util/cookie.go
+++ util/cookie.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,14 +14,22 @@ const (
 
 func Counter() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		value, err := context.Cookie(cookieKey)
-		if err == nil && value != "" {
-			count, _ := strconv.Atoi(value)
-			count++
-			cookieValue := strconv.Itoa(count)
-			context.SetCookie(cookieKey, cookieValue, 0, "", "", false, false)
+		cookie, err := context.Request.Cookie(cookieKey)
+		if err == nil {
+			if cookie.Path != "/" {
+				cookie.Path = "/"
+				cookie.Expires = time.Now().Add(4 * time.Hour)
+			}
+			cnt, _ := strconv.Atoi(cookie.Value)
+			cnt++
+			cookie.Value = strconv.Itoa(cnt)
+			http.SetCookie(context.Writer, cookie)
 		} else {
-			context.SetCookie(cookieKey, "0", 0, "", "", false, false)
+			http.SetCookie(context.Writer, &http.Cookie{
+				Name:    cookieKey,
+				Value:   "0",
+				Expires: time.Now().Add(4 * time.Hour),
+			})
 		}
 		context.Next()
 	}
